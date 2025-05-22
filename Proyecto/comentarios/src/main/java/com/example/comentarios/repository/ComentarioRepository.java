85b59cb71753ebc1f26232d7e2781809874e9dfc
MateoPortatil <mateocarballo2@gmail.com>
2025-05-22 22:51:37 +0200

    modificacion previa entrega, graphql no funciona. Modificado puerto Api-Gateway de 9000 al 8080

--- Proyecto/comentarios/src/main/java/com/example/comentarios/repository/ComentarioRepository.java
+++ Proyecto/comentarios/src/main/java/com/example/comentarios/repository/ComentarioRepository.java
@@ -2,8 +2,11 @@ package com.example.comentarios.repository;
 
 import com.example.comentarios.model.Comentario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface ComentarioRepository extends MongoRepository<Comentario, String> {
     Comentario findByUsuarioIdAndHotelIdAndReservaId(Integer usuarioId, Integer hotelId, Integer reservaId);
 }
